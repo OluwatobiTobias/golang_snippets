54e9dbd6b2c467cd4dcfd09e0187a7e0651549cc
OLuwatobiTobias <giwaoluwatobi@gmail.com>
2023-03-12 21:45:58 +0100

    webserver with chi

--- cmd/snippet_go/main.go
+++ cmd/snippet_go/main.go
@@ -1,47 +1,32 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
+	"flag"
 
-	"github.com/jackc/pgx/v4"
-	"github.com/joho/godotenv"
+	custom_middleware "github.com/OluwatobiTobias/golang_snippets/internal/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
+// var routes = flag.Bool("routedoc", false, "Generate router docs")
+
 func main() {
+	flag.Parse()
 
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("could not load .env by godotenv")
-	}
-	db_Conn, ok := os.LookupEnv("db_login")
-	if !ok {
-		fmt.Println("could not load .env by os package")
-	}
+	r := chi.NewRouter()
 
-	conn, err := pgx.Connect(context.Background(), db_Conn)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to connect\n %v\n", err)
-	}
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-	defer conn.Close(context.Background())
+	r.Route("/{articles}", func(r chi.Router) {
+		r.Use(custom_middleware.ArticleCtx)
+		// r.Get("/", GetArticle)
+		// 	r.Put("/", UpdateArticle)
+		// 	r.Delete("/", DeleteArticle)
+	})
 
-	if row, err := conn.Query(context.Background(), "select content from public.posts"); err != nil {
-		fmt.Println("query-level-error", err)
-
-	} else {
-
-		var man string
-
-		for row.Next() {
-
-			row.Scan(&man)
-			fmt.Println(man)
-
-		}
-		if row.Err() != nil {
-			fmt.Println("error is", err)
-		}
-	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.20
 require github.com/jackc/pgx/v4 v4.18.1
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/render v1.0.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
